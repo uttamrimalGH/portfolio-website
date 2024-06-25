1e92acd755bd28288089bb5b7d04e70af49e27b3
uttamrimalGH <159606568+uttamrimalGH@users.noreply.github.com>
2024-06-25 13:34:08 +0545

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleDropdown() {
-    const dropdownContent = document.getElementById("myDropdown");
-    dropdownContent.classList.toggle("show");
-}
-function toggleDropdown() {
-    const dropdownContent = document.getElementById("myDropdown");
-    dropdownContent.classList.toggle("show");
-}
